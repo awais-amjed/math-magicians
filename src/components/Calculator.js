d6f9f36478f90a8ccf31125fab5cd29e901c4181
Awais Amjed <awaisamjed171@gmail.com>
2022-03-16 17:35:42 +0500

    Add Styling to Calculator & buttons

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -13,24 +13,28 @@ const Calculator = () => (
       <CalculatorButton text="%" />
       <CalculatorButton text="÷" />
     </div>
+    <hr />
     <div className="calculator-row">
       <CalculatorButton text="7" />
       <CalculatorButton text="8" />
       <CalculatorButton text="9" />
       <CalculatorButton text="x" />
     </div>
+    <hr />
     <div className="calculator-row">
       <CalculatorButton text="4" />
       <CalculatorButton text="5" />
       <CalculatorButton text="6" />
       <CalculatorButton text="-" />
     </div>
+    <hr />
     <div className="calculator-row">
       <CalculatorButton text="1" />
       <CalculatorButton text="2" />
       <CalculatorButton text="3" />
       <CalculatorButton text="+" />
     </div>
+    <hr />
     <div className="calculator-row">
       <CalculatorButton text="0" takesDoubleSpace />
       <CalculatorButton text="." />
