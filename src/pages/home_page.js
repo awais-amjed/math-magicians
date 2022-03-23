13bf1d744403236dd65e40752156c58c81471554
Awais Amjed <awaisamjed171@gmail.com>
2022-03-23 20:47:13 +0500

    Add Quotes & Fix content padding for mobile

--- src/pages/home_page.js
+++ src/pages/home_page.js
@@ -8,7 +8,7 @@ const HomePage = () => (
     <Navbar />
     <div className="content">
       <div id="home-page">
-        <h2>- Welcome to Our Page -</h2>
+        <h2>Welcome to Our Page!</h2>
         <div id="welcome-images">
           <img src="https://media.giphy.com/media/QCdZ8ZtDe9uERX8XEv/giphy.gif" alt="gif" height="200" />
           <img src="https://media.giphy.com/media/YatSY0aqkm8UUQp69C/giphy.gif" alt="gif" height="200" />
--- src/pages/quote_page.js
+++ src/pages/quote_page.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import Navbar from '../components/navbar';
 import Footer from '../components/footer';
+import './quote_page.scss';
+import einstein from '../images/einstein.png';
+import galileo from '../images/galileo.png';
 
 const QuotePage = () => (
   <div>
     <Navbar />
+    <div className="content">
+      <div id="quote-page">
+        <h2>Big Brain Time!</h2>
+        <div id="quote-images">
+          <img src={galileo} alt="" height="350px" />
+          <img src={einstein} alt="" height="350px" />
+        </div>
+      </div>
+    </div>
     <Footer />
   </div>
 );
